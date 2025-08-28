4593a655339f51d4013f9996da4a3d0a79123920
levanduc <a519levanduc@gmail.com>
2025-08-28 15:07:39 -0500

    create api routes for workouts and update in server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutsRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({mssg: "welcoming"});
-})
+app.use('/api/workouts', workoutsRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
