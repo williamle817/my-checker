e3781da2e3f6a3d3ad90b8fec34bed2966aae31d
levanduc <a519levanduc@gmail.com>
2025-08-28 02:16:59 -0500

    setup port

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
+require('dotenv').config()
+
 const express = require('express')
+
+// express app
+const app = express()
+
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: "welcoming"});
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening to', process.env.PORT);
+});
+
+process.env
