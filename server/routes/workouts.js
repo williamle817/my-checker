de787e5d550fedc5028394e470ee233aac6574cb
levanduc <a519levanduc@gmail.com>
2025-08-28 17:23:57 -0500

    feat: complete workout controller with CRUD operations

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -2,7 +2,9 @@ const express = require('express')
 const { 
     getWorkouts,
     getWorkout,
-    createWorkout 
+    createWorkout,
+    deleteWorkout,
+    updateWorkout 
 } = require('../controllers/workoutController')
 
 const router = express.Router()
@@ -17,13 +19,9 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // delete a workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg: "delete a workout"});
-})
+router.delete('/:id', deleteWorkout)
 
 // update a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: "update a workout"});
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
